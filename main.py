f9901efba668eb9d1f351157e57786c6caaf1a44
Razvan377 <rey.rey.agapi@gmail.com>
2020-06-27 20:39:20 +0300

    0.1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random
+import time
 
 root = Tk()
 root.title("Sort Visualizer")
@@ -7,10 +8,16 @@ root.title("Sort Visualizer")
 c = Canvas(root, width=1200, height=700)
 c.grid()
 
+pos = 0
+array = []
+notSorted = True
+for num in range(201):
+    num = random.randrange(1, 201)
+    array.append(num)
+
 def GenerateArray():
     c.delete("all")
     position = 0
-    global array
     array = []
     for num in range(200):
         num = random.randrange(1,200)
@@ -20,14 +27,45 @@ def GenerateArray():
         position += 4
     #c.create_line(150, 1, 150, 200)
 
-def SelectionSort():
-    GenerateArray()
+def SelectionSort(arr):
+    # GenerateArray()
+    while (notSorted):
+        for i in range(200):
+            if (array[i] > array[i + 1]):
+                notSorted = False
+
+
+    smallest_index = findSmallest()
+    smallest = arr[smallest_index]
+    arr.pop(smallest_index)
+    arr.insert(pos, smallest)
+    addOne(pos)
+
+def addOne(x):
+    x+1
+
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def repaint(array):
+    c.delete("all")
+    position = 0
+    for num in array:
+        c.create_line(150+position,1,150+position,num*3)
+        position += 4
 
 
 generate_b = Button(root, text="Generate new array", command=GenerateArray)
 generate_b.grid(row=3, column=0)
 
-selectionSort_b = Button(root, text="Selection Sort", command=SelectionSort)
+selectionSort_b = Button(root, text="Selection Sort", command=SelectionSort(array))
 selectionSort_b.grid(row=3, column=1)
 
 # d = {}
@@ -36,6 +74,15 @@ selectionSort_b.grid(row=3, column=1)
 #     d[f"string{x}"] = "Hi"
 #
 # print(d["string5"])
+
+
+
+
+while(notSorted):
+    for i in range(200):
+        if (array[i] > array[i + 1]):
+            notSorted = False
+
 
 
 root.mainloop()
